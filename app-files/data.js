f89b216c6c74f2a0be6a4a24db495fea94594e7e
Kugaraya <infinitium.dev@gmail.com>
2026-08-15 03:02:05 +0800

    panorama view

---
+++ app-files/data.js
@@ -0,0 +1,818 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrance",
+      "name": "Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.10790911883956156,
+        "pitch": 0.038461588598675434,
+        "fov": 1.4501797619744572
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4323961793939777,
+          "pitch": -1.251755335144841,
+          "rotation": 0,
+          "target": "1-front-overview"
+        },
+        {
+          "yaw": 0.22619960520048288,
+          "pitch": -0.5504994577038111,
+          "rotation": 0,
+          "target": "2-top-overview"
+        },
+        {
+          "yaw": -0.11415422990877566,
+          "pitch": -0.008989751156704529,
+          "rotation": 0,
+          "target": "3-heritage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-front-overview",
+      "name": "Front Overview",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.22536339052813048,
+        "pitch": 0.3269296944501594,
+        "fov": 1.4501797619744572
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.111595064148677,
+          "pitch": 0.7828696754163502,
+          "rotation": 0,
+          "target": "0-entrance"
+        },
+        {
+          "yaw": 0.0734587390683128,
+          "pitch": -0.243610095267762,
+          "rotation": 0,
+          "target": "2-top-overview"
+        },
+        {
+          "yaw": -0.6278247074864183,
+          "pitch": 0.9813289858022891,
+          "rotation": 0,
+          "target": "3-heritage"
+        },
+        {
+          "yaw": 0.04020391686587743,
+          "pitch": 0.6577617604378645,
+          "rotation": 0,
+          "target": "4-village-center-1"
+        },
+        {
+          "yaw": 0.5525040684653568,
+          "pitch": 0.4336338126424817,
+          "rotation": 0,
+          "target": "7-road-crossing-1"
+        },
+        {
+          "yaw": 0.6740759001173622,
+          "pitch": 0.3236754299267517,
+          "rotation": 0,
+          "target": "8-road-crossing-2"
+        },
+        {
+          "yaw": 0.8550436770819019,
+          "pitch": 0.2289913321636199,
+          "rotation": 0,
+          "target": "12-open-ground"
+        },
+        {
+          "yaw": 0.27525446662703956,
+          "pitch": 0.19315078682342346,
+          "rotation": 0,
+          "target": "11-bridge-construction"
+        },
+        {
+          "yaw": 0.7041715687060233,
+          "pitch": 0.20321273192800327,
+          "rotation": 0,
+          "target": "9-road-crossing-3"
+        },
+        {
+          "yaw": 0.4645481741483444,
+          "pitch": 0.20041473429751377,
+          "rotation": 0,
+          "target": "10-road-crossing-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.08635059453370708,
+          "pitch": 0.4978594798862126,
+          "title": "3-Storey Balay Hu Museum",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.82276551380866,
+          "pitch": 0.7785838034142962,
+          "title": "Abaca Weaving House",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.0735250202766125,
+          "pitch": 0.7583106851959034,
+          "title": "Abaca Weaving House",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.3926814498997793,
+          "pitch": 1.0723014976085636,
+          "title": "Daraghuyan Heritage Center",
+          "text": "<span style=\"color: rgb(226, 229, 233); font-family: &quot;Segoe UI Historic&quot;, &quot;Segoe UI&quot;, Helvetica, Arial, sans-serif; font-size: 14px; white-space: pre-wrap; background-color: rgb(37, 39, 40);\">The Mt. Kitanglad Cultural Heritage Center, established in 2007 under the World Bank Small Grants for Indigenous Peoples</span>"
+        }
+      ]
+    },
+    {
+      "id": "2-top-overview",
+      "name": "Top Overview",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -0.1822338362141167,
+        "pitch": 1.5306150235663853,
+        "fov": 1.4501797619744572
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0814906361246983,
+          "pitch": 0.3931162597780151,
+          "rotation": 0,
+          "target": "0-entrance"
+        },
+        {
+          "yaw": 2.416048911977027,
+          "pitch": 0.7414315619112344,
+          "rotation": 0,
+          "target": "3-heritage"
+        },
+        {
+          "yaw": 2.133782014494889,
+          "pitch": -0.16109533284827648,
+          "rotation": 0,
+          "target": "1-front-overview"
+        },
+        {
+          "yaw": 2.348924365469431,
+          "pitch": 1.240541100103627,
+          "rotation": 0,
+          "target": "4-village-center-1"
+        },
+        {
+          "yaw": 0.3641503877494259,
+          "pitch": 0.8592203594776855,
+          "rotation": 0,
+          "target": "7-road-crossing-1"
+        },
+        {
+          "yaw": 0.11269117963437125,
+          "pitch": 0.5858443263672051,
+          "rotation": 0,
+          "target": "8-road-crossing-2"
+        },
+        {
+          "yaw": -0.06002151232945607,
+          "pitch": 0.35806967534410283,
+          "rotation": 0,
+          "target": "9-road-crossing-3"
+        },
+        {
+          "yaw": -0.4299265802668977,
+          "pitch": 0.3672122230758834,
+          "rotation": 0,
+          "target": "10-road-crossing-4"
+        },
+        {
+          "yaw": -0.7370545612186508,
+          "pitch": 0.3142758676072024,
+          "rotation": 0,
+          "target": "11-bridge-construction"
+        },
+        {
+          "yaw": 0.3173160480504258,
+          "pitch": 0.29926755938310734,
+          "rotation": 0,
+          "target": "12-open-ground"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-heritage",
+      "name": "Heritage",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -0.5950490930994548,
+        "pitch": 0.3855752882383463,
+        "fov": 1.4501797619744572
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9922119658144943,
+          "pitch": 0.12624978228481964,
+          "rotation": 0,
+          "target": "0-entrance"
+        },
+        {
+          "yaw": 1.9962939153128838,
+          "pitch": 0.09823862260526361,
+          "rotation": 0,
+          "target": "4-village-center-1"
+        },
+        {
+          "yaw": 2.2369082355270358,
+          "pitch": 0.04816842267431731,
+          "rotation": 0,
+          "target": "7-road-crossing-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3297657226794293,
+          "pitch": 0.21612597500987896,
+          "title": "Daraghuyan Heritage Center",
+          "text": "<span style=\"color: rgb(226, 229, 233); font-family: &quot;Segoe UI Historic&quot;, &quot;Segoe UI&quot;, Helvetica, Arial, sans-serif; font-size: 14px; white-space: pre-wrap; background-color: rgb(37, 39, 40);\">The Mt. Kitanglad Cultural Heritage Center, established in 2007 under the World Bank Small Grants for Indigenous Peoples</span>"
+        },
+        {
+          "yaw": 0.09650043018527299,
+          "pitch": 0.2812983277882797,
+          "title": "Abaca Weaving House",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.35468688783874214,
+          "pitch": 0.1673236919097434,
+          "title": "Abaca Weaving House",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-village-center-1",
+      "name": "Village Center-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6839228941924524,
+          "pitch": 0.04578927800003996,
+          "rotation": 0,
+          "target": "7-road-crossing-1"
+        },
+        {
+          "yaw": 1.7729697414301606,
+          "pitch": 0.16908355584252988,
+          "rotation": 0,
+          "target": "5-village-center-2"
+        },
+        {
+          "yaw": 0.5123758313294839,
+          "pitch": 0.14906019743763466,
+          "rotation": 0,
+          "target": "6-village-center-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8922766585828334,
+          "pitch": -0.17153498807476808,
+          "title": "3-Storey Balay Hu Museum",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.3076967762961562,
+          "pitch": 0.11607777831997268,
+          "title": "Daraghuyan Organization Inc., Pasalubong Center",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-village-center-2",
+      "name": "Village Center-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6830437062197028,
+          "pitch": 0.25069645527548623,
+          "rotation": 0,
+          "target": "4-village-center-1"
+        },
+        {
+          "yaw": -1.2263744463975534,
+          "pitch": 0.12109932867447526,
+          "rotation": 0,
+          "target": "3-heritage"
+        },
+        {
+          "yaw": -1.6642963382308267,
+          "pitch": 0.09920538059277817,
+          "rotation": 0,
+          "target": "0-entrance"
+        },
+        {
+          "yaw": 0.21952153982467948,
+          "pitch": 0.15973038815979557,
+          "rotation": 0,
+          "target": "6-village-center-3"
+        },
+        {
+          "yaw": 2.3824472597787647,
+          "pitch": 0.14951350938542163,
+          "rotation": 0,
+          "target": "7-road-crossing-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.02088175430471395,
+          "pitch": -0.15603247468155956,
+          "title": "3-Storey Balay Hu Museum",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.17379665272471811,
+          "pitch": 0.4343218759166394,
+          "title": "Daraghuyan Organization Inc., Pasalubong Center",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-village-center-3",
+      "name": "Village Center-3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.45013920654524675,
+          "pitch": 0.17777275489104483,
+          "rotation": 0,
+          "target": "4-village-center-1"
+        },
+        {
+          "yaw": -0.4222988580678084,
+          "pitch": 0.1834704756751897,
+          "rotation": 0,
+          "target": "5-village-center-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.07944429026812827,
+          "pitch": 0.30818737562217535,
+          "title": "3-Storey Balay Hu Museum",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "7-road-crossing-1",
+      "name": "Road Crossing-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.031221277025127,
+          "pitch": 0.12273658075720562,
+          "rotation": 0,
+          "target": "5-village-center-2"
+        },
+        {
+          "yaw": -0.11995165117639495,
+          "pitch": 0.12192805210749214,
+          "rotation": 0,
+          "target": "8-road-crossing-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-road-crossing-2",
+      "name": "Road Crossing-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.052020453212167,
+          "pitch": 0.20082355530177587,
+          "rotation": 0,
+          "target": "7-road-crossing-1"
+        },
+        {
+          "yaw": 0.8266408371155336,
+          "pitch": -0.012907972415490576,
+          "rotation": 0,
+          "target": "12-open-ground"
+        },
+        {
+          "yaw": -0.03512978128781086,
+          "pitch": 0.039741551631067296,
+          "rotation": 0,
+          "target": "9-road-crossing-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-road-crossing-3",
+      "name": "Road Crossing-3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9468870210726017,
+          "pitch": 0.044625332133508167,
+          "rotation": 0,
+          "target": "12-open-ground"
+        },
+        {
+          "yaw": -0.2659368187986271,
+          "pitch": 0.17813625484141404,
+          "rotation": 0,
+          "target": "10-road-crossing-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-road-crossing-4",
+      "name": "Road Crossing-4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06329290189650649,
+          "pitch": 0.21178544051729986,
+          "rotation": 0,
+          "target": "11-bridge-construction"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-bridge-construction",
+      "name": "Bridge Construction",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-open-ground",
+      "name": "Open Ground",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Daraghuyan Ancestral Domain - Tribal Village",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
